960d11289a30a04192a681643ffa771023abc094
Troels Irgens-Møller <troelsirgensmoeller@gmail.com>
2016-10-12 16:50:42 +0200

    Version bump, dependency update

--- releases/2.6.8/flux-react-2.6.8.js
+++ releases/2.6.8/flux-react-2.6.8.js
@@ -788,9 +788,7 @@ function mergeStore (mixins, source) {
 
   source.emit = function () {
     var args = arguments;
-//    setTimeout(function () { // Async to avoid running within render of component
-      exports.emit.apply(exports, args);
-//    }, 0);
+    exports.emit.apply(exports, args);
   };
 
   exports.addChangeListener = function (callback) {
